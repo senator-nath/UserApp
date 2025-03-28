da08fb230668f74f7a7e7a5a3473e6c6ebf5a740
senator-nath <nathanielosana18@gmail.com>
2025-03-28 10:54:45 +0100

    gitignore

--- controllers/userController.js
+++ controllers/userController.js
@@ -46,8 +46,8 @@ exports.updateUser = async (req, res) => {
 exports.deleteUser = async (req, res) => {
     try {
         const user = await User.findByIdAndDelete(req.params.id);
-        if (!user) return res.status(404).json({ message: "User not found" });
-        res.status(200).json({ message: "User deleted successfully" });
+        if (!user) return res.status(404).json({ message: "User not found." });
+        res.status(200).json({ message: "User deleted successfully." });
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
